42dee0653ebba5677645afac14c10c894bfa7c58
yunzhu <she21281@adobe.com>
2024-11-12 14:14:44 +0800

    carousel initiate

--- blocks/quote/quote.js
+++
@@ -1,7 +0,0 @@
-export default function decorate(block) {
-  const [quoteWrapper] = block.children;
-
-  const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
-}
